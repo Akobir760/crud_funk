ea1ca5a1c4ac5fc579c9e59bc485140c2371d198
Akobir <akobirortiqov40@gmail.com>
2025-12-10 17:52:17 +0500

    finish blog views

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import post_list, post_detail, post_update, post_delete, post_create
+
+urlpatterns = [
+    path('', post_list, name='post_list'),
+    path('post/<int:pk>/', post_detail, name='post_detail'),
+    path('create/', post_create, name='post_create'),
+    path('update/<int:pk>/', post_update, name='post_update'),
+    path('delete/<int:pk>/', post_delete, name='post_delete'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Post, Tag
+from .forms import PostForm
+from django.core.paginator import Paginator
 
-# Create your views here.
+
+def post_list(request):
+    tag_name = request.GET.get("tag")
+    posts = Post.objects.all().order_by('-created_at')
+
+    if tag_name:
+        posts = posts.filter(tags__name=tag_name)
+
+    paginator = Paginator(posts, 5)
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+
+    return render(request, 'templates/post_list.html', {"posts": posts})
+
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, id=pk)
+    return render(request, 'templates/post_detail.html', {"post": post})
+
+
+@login_required
+def post_create(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            form.save_m2m()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+
+    return render(request, 'templates/post_form.html', {"form": form})
+
+
+@login_required
+def post_update(request, pk):
+    post = get_object_or_404(Post, id=pk, author=request.user)
+    
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', pk=pk)
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, 'templates/post_form.html', {"form": form})
+
+
+@login_required
+def post_delete(request, pk):
+    post = get_object_or_404(Post, id=pk, author=request.user)
+    post.delete()
+    return redirect('post_list')
